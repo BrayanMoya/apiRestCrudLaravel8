7bb0c28b0872574d24697650873b49a60ef95f4c
bmoya <bmoya17@hotmail.com>
2022-09-25 20:28:05 -0500

    Feat: :bulb: * Se agrega libreria, rutas, configuraciones y controlador necesario para ejecución de autenticación por API con JWT

--- routes/api.php
+++ routes/api.php
@@ -19,6 +19,21 @@ Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
 });
 
+Route::group([
+
+    'middleware' => 'api',
+    'prefix' => 'auth'
+
+], static function ($router) {
+
+    Route::post('login', 'AuthController@login');
+    Route::post('logout', 'AuthController@logout');
+    Route::post('refresh', 'AuthController@refresh');
+    Route::post('profile', 'AuthController@profile');
+    Route::post('register', 'AuthController@register');
+
+});
+
 Route::get('/product', ProductController::class . '@index');
 Route::post('/product/new', ProductController::class . '@store');
 Route::put('/product/update/{id}', ProductController::class . '@update');
